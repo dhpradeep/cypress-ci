0d213414e906a70329490ba237720073b68f0742
dhpradeep <dhpradeep25@gmail.com>
2023-03-05 18:58:14 +0400

    added failed test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,4 +4,9 @@ describe('template spec', () => {
     cy.get("h1").should("contain", "Cypress Test")
     cy.get("p").should("contain", "From github CI")
   })
+
+    it('fails', () => {
+    cy.visit('/')
+    cy.get("h1").should("contain", "Failed")
+    })
 })
