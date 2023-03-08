8573d9ec8d248cf23960ead5e191e39845d33389
dhpradeep <dhpradeep25@gmail.com>
2023-03-09 00:37:15 +0400

    added env

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+        config.env = process.env
+        return config
       // implement node event listeners here
     },
     baseUrl: 'http://localhost:3000',
